ff50e55e4e0f47527d51139000f8b6b3d5180b82
azizbeAnvarjanov <https://github.com/azizbeAnvarjanov>
2022-10-02 13:46:47 +0500

    modified all elements

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import './Heade.css'
+import Img from '../../imgs/Azizbek.jpg'
+import { FaRegComment, FaTelegram } from 'react-icons/fa';
+import { GrInstagram } from 'react-icons/gr';
+import { BsGithub } from 'react-icons/bs';
+
+
+function Header() {
+
+
+
+  return (
+    <>
+        <div className="name">
+            <h1>Hi, I'm Anvarjanov Azizbek</h1>
+            <h1>Frontend Developer</h1>
+        </div>
+        <div className="header">
+            <div className="home_blog">
+                <div className='img_box'>
+                    <div className="img">
+                        <img src={Img} alt="" />
+                    </div>
+                      <div className="social_network">
+                          <a href="#"><FaTelegram /></a>
+                          <a href="#"><BsGithub /></a>
+                          <a href="#"><GrInstagram /></a>
+                      </div>
+                </div>
+                <div className='home_info_1'>
+                    <div>
+                        <h4 className='home_info_title'>BIOGRAPHY</h4>
+                          <p className='home_info_description'>Hi, I'm Anvarjanov Azizbek, Frontend Developer. Passionate about design beautiful web interfaces.</p>
+                    </div>
+                    <div>
+                        <h4 className='home_info_title'>CONTACT</h4>
+                          <p className='home_info_description'>
+                            Uzbekistan, Namangan <br />
+                            aanvarjanov52gmail.com <br />
+                            <a href='tel:+998882547775'>+998 88 254 77 75</a>
+                          </p>
+                    </div>
+                    <div>
+                        <h4 className='home_info_title'>LANGUAGES</h4>
+                          <p className='home_info_description'>
+                            Uzbek - Advansed <br />
+                            Russian - Advansed <br />
+                            English - Beginner
+                          </p>
+                    </div>
+                </div>
+                <div className='home_info_2'>
+                      <div>
+                          <h4 className='home_info_title'>PROJECTS</h4>
+                          <h2 className='home_info_description'>1</h2>
+                      </div>
+                      <div>
+                          <h4 className='home_info_title'>COUNTRY</h4>
+                          <p className='home_info_description'>
+                            Uzbekistan, Namangan
+                          </p>
+                      </div>
+                      <div>
+                          <h4 className='home_info_title'>RESUME</h4>
+                          <a href='#' className='home_info_description'>
+                            Download Rezume
+                          </a>
+                      </div>
+                </div>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+<div>
+    <p className='info_p'>
+        <span>BIOGRAPHY</span> <br /> <br />
+        Hi, I'm Anvarjanov Azizbek, Frontend Developer. Passionate about design beautiful web interfaces.
+    </p>
+    <p>
+        <span>CONTACT</span> <br /> <br />
+        <p>Uzbekistan, Namangan</p>
+        <p>aanvarjanov52gmail.com</p>
+        <a href='tel:+998882547775'>+998 88 254 77 75</a>
+    </p>
+    <p>
+        <span>LANGUAGES</span> <br /> <br />
+        <p>Uzbek - Advansed</p>
+        <p>Russian - Advansed</p>
+        <p>English - Beginner</p>
+    </p>
+</div>
+
+export default Header
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import { FiMoon, FiSun } from 'react-icons/fi';
+import './Navbar.css';
+import { FaRegComment } from 'react-icons/fa';
+import { HiHome } from 'react-icons/hi';
+import { BiCollection, BiCategory } from 'react-icons/bi'
+import { BsBook } from 'react-icons/bs'
+
+function Navbar() {
+
+    const [mode, setMode] = useState('dark');
+    const [navbar, setNavbar] = useState('hide_navbar');
+    const [navbarActive, setNavbarActive] = useState(false);
+    
+    function scrollNavbar() {
+        window.scrollY > 100 ? setNavbarActive(true) : setNavbarActive(false);
+    }
+
+
+    function addClassLight() {
+        setMode('dark');
+        document.querySelector('body').classList.remove('active_body');
+    }
+    function addClassDark() {
+        setMode('light');
+        document.querySelector('body').classList.add('active_body');
+    }
+
+    function show_navbar() {
+        navbar === "hide_navbar" ? setNavbar('show_navabr') : setNavbar('hide_navbar');
+    }
+
+    window.addEventListener('scroll', () => {
+        scrollNavbar();
+    });
+
+  return (
+    <>
+        <nav className={navbarActive ? 'navbar_active' : ''}>
+            <h1 className='desktop_logo'>Azizbek</h1>
+            <ul className={navbar === 'hide_navbar' ? 'hide_navbar' : 'show_navbar'}>
+                <li>
+                    <a onClick={() => show_navbar()} href="#"><HiHome className='link_icon' />  Home</a>
+                </li>
+                <li>
+                      <a onClick={() => show_navbar()} href="#"><BiCollection className='link_icon' />  Skills</a>
+                </li>
+                <li>
+                    <a onClick={() => show_navbar()} href="#"><BsBook className='link_icon' />  Qualification</a>
+                </li>
+                <li className='phone_logo'><a href="#"><h1>Azizbek</h1></a></li>
+                <li>
+                      <a onClick={() => show_navbar()} href="#"><BiCategory className='link_icon' />  Projects</a>
+                </li>
+                <li>
+                    <a onClick={() => show_navbar()} href="#"><FaRegComment className='link_icon' />  Contact</a>
+                </li>
+                <div className="dark_mode">
+                    <FiMoon onClick={() => addClassDark()} className={mode === 'dark' ? 'active_mode' : ''}/>
+                    <FiSun onClick={() => addClassLight()} className={mode === 'light' ? 'active_mode' : '' }/>
+                </div>
+            </ul>
+              <div className={navbar === 'hide_navbar' ? 'menu' : 'menu active_menu'} onClick={() => show_navbar()}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
