1630487e3b6c66c0bd6ab1632984b33d31530ddb
azizbeAnvarjanov <https://github.com/azizbeAnvarjanov>
2022-10-04 17:31:42 +0500

    d

--- src/App.js
+++ src/App.js
@@ -7,11 +7,32 @@ import Project from './Components/Project/Project';
 import Contact from './Components/Contact/Contact';
 import Footer from "./Components/Footer/Footer";
 
+// icons
+import { BsArrowUpSquare } from 'react-icons/bs';
+
+// css
+import './App.css';
+
+// up_btn function
+function up_btn() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      document.querySelector('.up_btn').classList.add('active');
+    }else{
+      document.querySelector('.up_btn').classList.remove('active');
+    }
+});
 
 
 function App() {
   return (
     <>
+      <BsArrowUpSquare className="up_btn" onClick={up_btn} />
       <Navbar />
       <Header />
       <Skills />
